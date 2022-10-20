937d44cf22d1f353c563ea75ad07eed4fd661f79
julian salazar <julian456@gmail.com>
2022-10-20 20:19:39 -0300

    Simulador reservas de alojamientos temporarios

---
+++ preentrega2.js
@@ -0,0 +1,95 @@
+
+class Alojamiento{
+    constructor(nombre , precio_noche , noches_disponibles){
+
+        this.nombre = nombre;
+        this.precio_noche = precio_noche;
+        this.noches_disponibles = noches_disponibles;
+    
+    }
+
+    get_datos(){
+        console.log(" ");
+        console.log("Alojamiento: " , this.nombre);
+        console.log("Precio:" , this.precio_noche);
+    }
+
+    get_noches_disponibies(){
+
+        if(this.noches_disponibles < 3){
+            return false
+        } 
+        else{
+            return true
+        }
+    }
+
+    update_noches_disponibles(cantidad){
+        this.noches_disponibles = this.noches_disponibles - cantidad;
+    }
+
+
+    }
+
+
+function buscar_alojamiento(alojamiento){{
+
+    return alojamiento.nombre == reserva_usuario;
+}}
+
+
+// Herramienta para el propietario 
+
+// Configuración de precios y disponibilidad de alojamientos 
+
+let listado_alojamientos = [];
+
+for(let i=0 ; i < 2 ; i++){
+
+    let nombre = prompt("Ingrese nombre del alojamiento");
+    let precio = prompt("Ingrese el precio por noche");
+    let noches_disponibles = prompt("Ingrese cantidad de noches disponibles en agenda");
+    
+    let alojamiento = new Alojamiento(nombre, precio, noches_disponibles);
+
+    listado_alojamientos.push(alojamiento);
+
+}
+console.log(listado_alojamientos);
+
+
+// Herramientas para el usuario / huésped
+
+// Visualización de alojamientos diponibles
+
+for(let alojamiento of listado_alojamientos){
+
+    alojamiento.get_datos();
+
+}
+
+// Reserva de alojamiento
+
+let reserva_usuario = prompt("Ingrese el alojamiento deseado");
+
+let resultado_find = listado_alojamientos.find(buscar_alojamiento);
+
+
+if( resultado_find != undefined){
+
+    if(resultado_find.get_noches_disponibies()){
+        
+        let noches = prompt("Indique cantidad de noches a reservar");
+        resultado_find.update_noches_disponibles(noches);
+        
+        if(noches < 3){
+
+        }
+    }
+    else{
+        console.log("No hay noches disponibles para este establecimiento");
+    }
+}
+
+console.log( resultado_find);
+
